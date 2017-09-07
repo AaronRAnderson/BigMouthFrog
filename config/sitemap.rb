e4f830068a670a1af689ee0039a190519b5ed688
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-09-07 09:52:33 -0600

    updated sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,9 +1,13 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.bigmouthfrogdev.com"
+SitemapGenerator::Sitemap.default_host = "https://www.bigmouthfrogdev.com"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
-  #
+  add root_path, :changefreq => 'daily'
+  add about_path, :changefreq => 'daily'
+  add contact_path, :changefreq => 'daily'
+  add featured_clients_path, :changefreq => 'daily'
+
   # The root path '/' and sitemap index file are added automatically for you.
   # Links are added to the Sitemap in the order they are specified.
   #
